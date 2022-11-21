a61e3eb78772e253b3493526bee3e4509121413b
EugeneGpil <gpilligrim@gmail.com>
2022-11-21 19:00:24 +0300

    animate init

--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -10,6 +10,23 @@ import { ref, onMounted } from "vue";
 const canvas = ref(null);
 
 onMounted(() => {
+  canvas.value.width = 1024;
+  canvas.value.height = 576;
+
   const c = canvas.value.getContext("2d");
+
+  let y = 100;
+
+  const animate = () => {
+    window.requestAnimationFrame(animate);
+
+    c.fillStyle = "#555";
+    c.fillRect(0, 0, canvas.value.width, canvas.value.height);
+
+    c.fillStyle = "red";
+    c.fillRect(200, y++, 100, 100);
+  };
+
+  animate();
 });
 </script>
