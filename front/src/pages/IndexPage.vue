64fcd23bb06cf157f068fe01bb7b6c29fb423cd1
EugeneGpil <gpilligrim@gmail.com>
2022-11-23 19:11:34 +0300

    gravity

--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -15,7 +15,41 @@ onMounted(() => {
 
   const c = canvas.value.getContext("2d");
 
-  let y = 100;
+  const gravity = 0.15;
+
+  class Player {
+    constructor({ x, y }) {
+      this.position = { x, y };
+      this.velocity = { x: 0, y: 1 };
+
+      this.height = 100;
+      this.width = 100;
+    }
+
+    draw() {
+      c.fillStyle = "red";
+      c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update() {
+      this.draw();
+      this.position.x += this.velocity.x;
+
+      if (
+        this.position.y + this.height + this.velocity.y <
+        canvas.value.height
+      ) {
+        this.position.y += this.velocity.y;
+        this.velocity.y += gravity;
+      } else {
+        this.velocity.y = 0;
+        this.position.y = canvas.value.height - this.height;
+      }
+    }
+  }
+
+  const player = new Player({ x: 100, y: 150 });
+  const player2 = new Player({ x: 200, y: 200 });
 
   const animate = () => {
     window.requestAnimationFrame(animate);
@@ -23,8 +57,8 @@ onMounted(() => {
     c.fillStyle = "#555";
     c.fillRect(0, 0, canvas.value.width, canvas.value.height);
 
-    c.fillStyle = "red";
-    c.fillRect(200, y++, 100, 100);
+    player.update();
+    player2.update();
   };
 
   animate();
