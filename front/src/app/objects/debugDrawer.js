b99a5a424c99ca89441e5b73aa160258c10ae446
EugeneGpil <gpilligrim@gmail.com>
2023-01-19 17:50:36 +0300

    1

--- front/src/app/objects/debugDrawer.js
+++ front/src/app/objects/debugDrawer.js
@@ -2,6 +2,10 @@ import globals from "src/app/objects/globals";
 
 export default {
   drawBackground({ object }) {
+    if (!import.meta.env.VITE_VISUALISE_PLAYER_BLOCKS) {
+      return;
+    }
+
     globals.c.fillStyle = "rgba(0, 0, 255, 0.1)";
     globals.c.fillRect(
       object.position.x,
@@ -12,6 +16,10 @@ export default {
   },
 
   drawHitbox({ object }) {
+    if (!import.meta.env.VITE_VISUALISE_PLAYER_BLOCKS) {
+      return;
+    }
+
     globals.c.fillStyle = "rgba(255, 0, 0, 0.1)";
     globals.c.fillRect(
       object.hitbox.position.x,
