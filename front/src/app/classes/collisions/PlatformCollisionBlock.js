7ede7a000b204179296d96320b2fbd3429f33eac
EugeneGpil <gpilligrim@gmail.com>
2023-01-19 17:58:16 +0300

    1

--- front/src/app/classes/collisions/PlatformCollisionBlock.js
+++ front/src/app/classes/collisions/PlatformCollisionBlock.js
@@ -4,6 +4,8 @@ export default class PlatformCollisionBlock extends CollisionBlock {
   constructor(params) {
     super(params);
 
+    this.height = 4;
+
     this.fillStyle = "rgba(0, 255, 0, 0.2)";
   }
 }
--- front/src/app/functions/collisions/detectCollisions/getCollisionWithPlatform.js
+++ front/src/app/functions/collisions/detectCollisions/getCollisionWithPlatform.js
@@ -2,8 +2,19 @@ import detectCollisionsWithCollection from "src/app/functions/collisions/detectC
 import platformArray from "src/app/arrays/collisions/types/platformArray";
 
 export default ({ object }) => {
+  const objHitbox = object.hitbox;
+
+  const bottomHitbox = {
+    position: {
+      x: objHitbox.position.x,
+      y: objHitbox.position.y + objHitbox.height - 1,
+    },
+    width: objHitbox.width,
+    height: 1,
+  };
+
   return detectCollisionsWithCollection({
-    object: object.hitbox,
+    object: bottomHitbox,
     objectsCollection: platformArray,
   });
 };
