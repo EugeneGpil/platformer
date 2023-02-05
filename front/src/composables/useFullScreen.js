2f6a36b56bc5b065b7f318a8ee1672834ac93ad4
EugeneGpil <gpilligrim@gmail.com>
2023-02-05 13:06:03 +0300

    1

--- front/src/composables/useFullScreen.js
+++ front/src/composables/useFullScreen.js
@@ -3,13 +3,15 @@
 // works only by pushing button, not in onMounted() and so on
 
 import { useQuasar } from "quasar";
-import unfocus from "src/composables/helpers/useUnfocus";
+import { useUnfocus } from "src/composables/helpers/useUnfocus";
+import globals from "src/app/objects/globals";
 
 export const useFullScreen = () => {
   const $q = useQuasar();
+  const unfocus = useUnfocus();
 
   return {
-    async toggle(target) {
+    async toggle() {
       if ($q.fullscreen.isActive) {
         await $q.fullscreen.exit();
         unfocus();
@@ -17,7 +19,7 @@ export const useFullScreen = () => {
         return;
       }
 
-      await $q.fullscreen.request(target.value);
+      await $q.fullscreen.request(globals.playScreen.value);
       unfocus();
     },
 
--- front/src/composables/useWindowActions.js
+++ front/src/composables/useWindowActions.js
@@ -1,4 +1,5 @@
 import { useUnfocus } from "src/composables/helpers/useUnfocus";
+import { useFullScreen } from "src/composables/useFullScreen";
 
 export const useWindowActions = () => {
   const unfocus = useUnfocus();
@@ -9,9 +10,21 @@ export const useWindowActions = () => {
     };
   };
 
+  const fullScreen = useFullScreen();
+
   return {
     minimize: getFunction(window.myWindowAPI?.minimize),
-    toggleMaximize: getFunction(window.myWindowAPI?.toggleMaximize),
+    toggleMaximize: () => {
+      if (fullScreen.isActive()) {
+        fullScreen.toggle()
+        getFunction(window.myWindowAPI?.toggleMaximize)()
+
+        return
+      }
+
+      getFunction(window.myWindowAPI?.toggleMaximize)()
+      fullScreen.toggle()
+    },
     closeApp: getFunction(window.myWindowAPI?.close),
   };
 };
